7a23e88c8e2fede847e3ce32940d1e9b75cd9a79
Lamooooo1418 <lamooooo1418@gmail.com>
2020-03-30 20:05:15 +0300

    Create Hospital_app

---
+++ hospital/db/migrate/20200330153704_create_patients.rb
@@ -0,0 +1,13 @@
+class CreatePatients < ActiveRecord::Migration[5.2]
+  def change
+    create_table :patients do |t|
+      t.primary_key :id
+      t.string :first_name
+      t.string :last_name
+      t.string :diagnosis
+      t.date :born_on
+
+      t.timestamps
+    end
+  end
+end
---
+++ hospital/db/seeds.rb
@@ -0,0 +1,14 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+Patient.create(id:1, first_name "Hello ",last_name:"", diagnosis:"Hr", born_on:02/02/2020)
+
+Patient.create(id:2, first_name "Hello ",last_name:"", diagnosis:"Hr", born_on:02/02/2020)
+
