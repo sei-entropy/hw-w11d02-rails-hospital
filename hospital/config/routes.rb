552ac58ca4fd9d01d87619723452b35397e84078
Lamooooo1418 <lamooooo1418@gmail.com>
2020-03-30 22:45:14 +0300

    Show

--- hospital/config/routes.rb
+++ hospital/config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+ 
+  #get 'patients/new' , to:'patients#new'
+
+  #get 'todos/:id' , to: 'todos#show'  
+  
+  get 'patients/:id', to: 'patients#show', as: 'patient'
+  #get 'patients', to: 'patients#index'
+  
+  #post 'patients', to: 'patients#create'
+  
+  #get 'pages/home'
+  #get 'pages/about'
+  #get 'pages/contact'
+  #get 'about', to: 'pages#about'
+  #get 'contact', to: 'pages#contact'
+
+  #root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
