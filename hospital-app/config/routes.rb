27c9b9c8f7cbb3c709b6dad2c5e2d69519dc891c
FaisalAljazaeri <faisal.hassan93@gmail.com>
2020-03-31 01:56:52 +0300

    Create Patients Controller

--- hospital-app/config/routes.rb
+++ hospital-app/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'patients', to: 'patients#index'
+  get 'patients/:id', to: "patients#show"
+  get 'patients/destroy'
+  get 'patients/update'
+  get 'patients/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
