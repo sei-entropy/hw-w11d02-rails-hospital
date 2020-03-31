db15f3c46e46d8b1f5e9588c8d95ac65aec0bd21
ranaalmalki <ranaalmalki12@gmail.com>
2020-03-31 09:50:36 +0300

    add CRUD

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+2020-03-30
+Patient.create(first_name:'Rana' ,last_name:'Almalki', diagnosis:"Common cold", born_on:"1996-11-18")
+Patient.create(first_name:'Hisham', last_name:'Aljahbli' ,diagnosis:"the bash", born_on:"1990-05-21")
+Patient.create(first_name:'Saqar', last_name:'Alarifi', diagnosis:"warning rails", born_on:"1993-01-01")
+Patient.create(first_name:'Rawan', last_name:'Alrajeh', diagnosis:"Common cold", born_on:"1996-02-02")
+Patient.create(first_name:'Haya', last_name:' abdulaziz', diagnosis:"Common cold", born_on:"1996-03-03")
