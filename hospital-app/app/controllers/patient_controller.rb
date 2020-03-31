4710cd827aefe7fd001c07fbc7ba92715747c0db
Ranen-Khlabi <Ranen-Alkhlabi@hotmail.com>
2020-03-31 11:22:56 +0300

    done hw

---
+++ hospital-app/app/controllers/patient_controller.rb
@@ -0,0 +1,29 @@
+class PatientsController < ApplicationController
+    def show
+   @patient = Patient.find(params[:id])
+   end
+   def index 
+     @patients = Patient.all  
+   end
+   def new
+       @patient = Patient.new
+   end
+   def create 
+       @patient = Patient.new(patient_params)
+       @patient.save
+       redirect_to @patient
+   end
+   def edit
+   @patient = Patient.find(params[:id])
+ end
+   def destroy
+ @patient = Patient.find(params[:id])
+ @patient.destroy
+ redirect_to @patient
+end
+   private 
+   def patient_params
+       params.require(:patient).permit(:first_name,:last_name,:diagnosis)
+   end
+
+end
