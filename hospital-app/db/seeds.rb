14a00e5f86713f52576ffd43a8c14071a46d6303
Amirah Almani <amirahalmani@gmail.com>
2020-04-01 01:07:30 +0300

    added doctor seed

--- hospital-app/db/seeds.rb
+++ hospital-app/db/seeds.rb
@@ -8,3 +8,8 @@
 Patient.create(first_name: "Sara", last_name: "Mohammed", diagnosis: "headache", born_on: "1999-02-2")
 Patient.create(first_name: "Nora", last_name: "Ahmed", diagnosis: "headache", born_on: "2000-08-01")
 Patient.create(first_name: "Yara", last_name: "khalid", diagnosis: "headache", born_on: "1996-01-10")
+
+
+Doctor.create(first_name: "layla", last_name: "Mohammed")
+Doctor.create(first_name: "huda", last_name: "Ahmed")
+Doctor.create(first_name: "nor", last_name: "khalid")
