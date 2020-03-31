5848a7b404cdc59a3af37327ad11d641d658bf45
aaljamid <aaljamid@emich.edu>
2020-04-01 00:49:45 +0300

    adding new and create

--- hospital-app/app/controllers/patients_controller.rb
+++ hospital-app/app/controllers/patients_controller.rb
@@ -7,6 +7,25 @@ class PatientsController < ApplicationController
     def show
         @patient = Patient.find(params[:id])
       end
+
+      def new
+        @patient = Patient.new
+      end
+
+      def create
+        @patient = Patient.new(patient_params)
+    
+        if @patient.save
+            redirect_to @patient
+          else
+            render 'new'
+          end
+      end
+    
+    private
+      def patient_params
+        params.require(:patient).permit(:first_name, :last_name, :diagnosis , :born_on)
+      end
 end
 
 
