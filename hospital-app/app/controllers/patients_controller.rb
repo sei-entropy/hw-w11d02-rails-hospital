2749961c28ddd2fb7a3d4cc7599ca5f8bef9a42e
malsaadan <mmalsaadan@gmail.com>
2020-03-31 10:37:23 +0300

    Extract params as private method

--- hospital-app/app/controllers/patients_controller.rb
+++ hospital-app/app/controllers/patients_controller.rb
@@ -12,7 +12,7 @@ class PatientsController < ApplicationController
   end
 
   def create
-    Patient.create(params.require(:patient).permit(:first_name, :last_name, :diagnosis, :born_on))
+    Patient.create(patient_params)
     redirect_to patients_path
   end
 
@@ -22,7 +22,7 @@ class PatientsController < ApplicationController
 
   def update
     patient = Patient.find(params[:id])
-    patient.update(params.require(:patient).permit(:first_name, :last_name, :diagnosis, :born_on))
+    patient.update(patient_params)
     redirect_to patient
   end
 
@@ -30,4 +30,9 @@ class PatientsController < ApplicationController
     Patient.find(params[:id]).destroy
     redirect_to patients_path
   end
+
+  private 
+    def patient_params
+      params.require(:patient).permit(:first_name, :last_name, :diagnosis, :born_on)
+    end
 end
