fca515d0365a5acfa3b6d815296d467e97fa4f69
wejdann <weejdansaud58@hotmail.com>
2020-03-31 18:36:05 +0300

    HomeWork

---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,25 @@
+class PatientsController < ApplicationController
+def show 
+    @patient = Patient.find(params[:id])
+end
+def index
+    @patients = Patient.all 
+end 
+def new 
+    @patient = Patient.new
+end 
+def create 
+    @patient = Patient.new(patient_params)
+    @patient.save
+    redirect_to @patient
+
+end 
+private 
+def patient_params
+params.require(:patient).permit(:first_name, :last_name, :diagnosis, :born_on )
+end 
+
+
+
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+ get 'patients/new', to: 'patients#new'
+ get 'patients/:id' , to: 'patients#show' , as: 'patient'
+ post 'patients', to: 'patients#create'
+ get 'patients' , to: 'patients#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
