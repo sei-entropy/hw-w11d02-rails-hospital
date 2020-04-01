7a986f50e8ac9842c10be6b5edc573a3fe761295
Renad-Ahmad <rena.34end@gmail.com>
2020-04-01 08:48:26 +0300

    Doctors AddDoctorToPatients

--- hospital-app/db/schema.rb
+++ hospital-app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_01_033649) do
+ActiveRecord::Schema.define(version: 2020_04_01_054322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,9 @@ ActiveRecord::Schema.define(version: 2020_04_01_033649) do
     t.date "born_on"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "doctor_id"
+    t.index ["doctor_id"], name: "index_patients_on_doctor_id"
   end
 
+  add_foreign_key "patients", "doctors"
 end
