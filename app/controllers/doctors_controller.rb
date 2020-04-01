e34961cfa422b4bb6255beb5fc009e535018881c
robaarishi24 <robaarishi@hotmail.com>
2020-04-01 19:27:36 +0300

    Done

---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,44 @@
+class DoctorsController < ApplicationController
+    def index 
+        @doctors=Doctor.all
+    end 
+
+    def show 
+        @doctor=Doctor.find(params[:id])
+        @patients=@doctor.patients
+    end 
+
+    def new 
+        @doctor = Doctor.new
+    end
+
+    def edit
+        @doctor=Doctor.find(params[:id])
+    end 
+
+    def create
+        @doctor = Doctor.new(doctor_params)
+        @doctor.save
+        redirect_to @doctor
+    end
+
+    def update 
+        @doctor=Doctor.find(params[:id])
+
+         @doctor.update(doctor_params)
+        redirect_to @doctor
+
+    end
+
+    def destroy
+         @doctor =Doctor.find(params[:id]).destroy
+            redirect_to doctors_path
+    end
+
+    private 
+    def patient_params
+        params.require(:doctor).permit(:first_name , :last_name , :zip_code ,:specialty )
+    end
+
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_31_145228) do
+ActiveRecord::Schema.define(version: 2020_04_01_160541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "doctors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "zip_code"
+    t.string "specialty"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "patients", force: :cascade do |t|
     t.string "first_name"
