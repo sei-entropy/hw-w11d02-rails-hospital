0e9f899de06531e9f596808bd700818a5aa520a5
Najwab <inajjwa2@gmail.com>
2020-04-01 09:24:51 +0300

    solved

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_31_194605) do
+ActiveRecord::Schema.define(version: 2020_04_01_055840) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,9 @@ ActiveRecord::Schema.define(version: 2020_03_31_194605) do
     t.date "born_on"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "doctor_id"
+    t.index ["doctor_id"], name: "index_patients_on_doctor_id"
   end
 
+  add_foreign_key "patients", "doctors"
 end
